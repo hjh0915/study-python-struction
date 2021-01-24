bf34717ba46c046eef219271c41420a344eafb29
hjh0915 <623299157@qq.com>
2021-01-24 18:51:49 +0800

    单独提取出队列、栈类

---
+++ basic/stack.py
@@ -0,0 +1,21 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        # self.items.append(item)
+        self.items.insert(0, item)
+    
+    def pop(self):
+        # return self.items.pop()
+        return self.items.pop(0)
+
+    def peek(self):
+        # return self.items[len(self.items)-1]
+        return self.items[0]
+    
+    def size(self):
+        return len(self.items)
